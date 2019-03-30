9324aa181b4d0a0c2000388328a6262e2446d856
RabiaSajjad <rabiasajjad.dev@gmail.com>
2019-03-30 16:37:58 -0400

    custom module merge nodes

--- html/modules/custom/mergenodes/src/Form/DefaultForm - Copy.php
+++ html/modules/custom/mergenodes/src/Form/DefaultForm - Copy.php
@@ -42,14 +42,14 @@ class DefaultForm extends FormBase {
 
     if ($form_state->isSubmitted()) {
       // get selected content type
-      $content_type = $form_state->getValue('contenttype');
-      if (empty($content_type)) {
+      $contenttye = $form_state->getValue('contenttype');
+      if (empty($contenttye)) {
         drupal_set_message("No content type selected for mapping", 'warning');
       }
       else {
         // fetch all nodes of content type
         $query = \Drupal::entityQuery('node');
-        $query->condition('type', $content_type);
+        $query->condition('type', $contenttye);
         $query->exists('field_previousnodeid');
         $query->sort('field_previousnodeid');
         $query->sort('langcode');
@@ -114,40 +114,48 @@ class DefaultForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $content_type = $form_state->getValue('contenttype');
-    if (empty($content_type)) {
-      drupal_set_message('No content type selected to merge', 'warning');
-    }
-    else {
+    // Display result.
+    /*foreach ($form_state->getValues() as $key => $value) {
+      drupal_set_message($key . ': ' . $value);
+    }*/
+    $contenttye = $form_state->getValue('contenttype');
+
+    if ($contenttye) {
       // 1. Fetch all nodes of content type
       $query = \Drupal::entityQuery('node');
-      $query->condition('type', $content_type);
+      $query->condition('type', $contenttye);
       $query->exists('field_previousnodeid');
       $query->sort('field_previousnodeid');
       $query->sort('langcode');
       $nids = $query->execute();
       $keys = array_keys($nids);
-      $storage_handler = \Drupal::entityTypeManager()->getStorage("node");
+//      drupal_set_message('Nodes = ' . print_r($nids, TRUE));
+//      drupal_set_message('Node vid = ' . print_r($keys, TRUE));
+//      drupal_set_message('Node 1 = ' . print_r($nids[$keys[0]], TRUE));
 
       for($i = 0; $i < sizeof($keys); $i++) {
         // 2. Load the field_previousnodeid for the first node
+        $storage_handler = \Drupal::entityTypeManager()->getStorage("node");
         $node1 = $storage_handler->load($nids[$keys[$i]]);
         $node1_previousnodeid = $node1->get('field_previousnodeid')->getValue();
         $node1_langcode = $node1->language()->getId();
+//        drupal_set_message('Node ' . $i . ': Node id = ' . $node1->id() . ' Previous node id = ' . $node1_previousnodeid[0]['value'] . ' Langcode = ' . $node1_langcode);
 
         // 3. check the field_previousnodeid for the next node
         $node2 = $storage_handler->load($nids[$keys[$i+1]]);
         if ($node2) {
           $node2_previousnodeid = $node2->get('field_previousnodeid')->getValue();
           $node2_langcode = $node2->language()->getId();
+//          drupal_set_message('Node ' . $i+1 . ': Node id = ' . $node2->id() . ' Previous node id = ' . $node2_previousnodeid[0]['value'] . ' Langcode = ' . $node2_langcode);
 
-          if (($node1_previousnodeid[0]['value'] == $node2_previousnodeid[0]['value'])
-            and ($node1_langcode != $node2_langcode)) {
+          if ($node1_previousnodeid[0]['value'] == $node2_previousnodeid[0]['value']) {
             // 4. If both have same value for field_previousnodeid merge the second node as a translation and delete after merging
-            $node1->addTranslation($node2_langcode, $node2->getTranslation($node2_langcode)->toArray());
-            $node1->save();
-            $node2->delete();
-            drupal_set_message('Merged translation' . $node2->get('title')->value . ' with ' . $node1->get('title')->value );
+//            drupal_set_message('Execute merge');
+//            $node1->addTranslation($node2_langcode, $node2->getTranslation($node2_langcode)->toArray());
+//            $node1->save();
+//            drupal_set_message('Translation added to node' . $node1->id());
+//            drupal_set_message('Removed node' . $node2->id());
+//            $node2->delete();
 
             // move counter one ahead
             $i++;
@@ -156,7 +164,13 @@ class DefaultForm extends FormBase {
             drupal_set_message('No translation found for ' . $node1->get('title')->value, 'error');
           }
         }
-      }
+/*        else {
+          drupal_set_message('Next node not found');
+        }
+*/      }
+    }
+    else {
+      drupal_set_message('No content type selected to merge', 'warning');
     }
 
   }
